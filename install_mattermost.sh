16014c5996b87ae95c963a1761e1b2071848015e
root <li.s@sfeir.com>
2024-04-15 09:43:09 +0200

    Mise à jour

---
+++ install_mattermost.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+sudo apt-get update
+sudo apt install gnome-terminal  # For non-Gnome Desktop environments
+sudo apt remove docker-desktop   # Remove previous Docker versions (if installed)
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh ./get-docker.sh
+sudo apt-get install docker-compose-plugin
+sudo apt-get install git
+docker run --name mattermost-preview -d --publish 8065:8065 mattermost/mattermost-preview
+
+
+EOF
---
+++ main.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4086ca08004eed1c2dfc3dd6b63bdf247c2886769d453f427cc57f236d42132a
+size 1631
