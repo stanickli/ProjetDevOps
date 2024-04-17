81964d1e3e28737f55468625aaabc438075657d2
root <li.s@sfeir.com>
2024-04-17 13:24:29 +0200

    Sonarqube et Trivy

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -21,4 +21,16 @@ sudo apt-get update -y
 sudo apt-get install jenkins -y
 sudo systemctl start jenkins
 
+#install docker sonarqube
+sudo apt-get install docker.io -y
+sudo chmod 777 /var/run/docker.sock
+sudo docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
+
+#install trivy
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy -y
+
 EOF
