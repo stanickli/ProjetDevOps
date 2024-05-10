f8f23a264bd96d858ca46dda9df346697099277f
root <li.s@sfeir.com>
2024-05-10 16:51:23 +0200

    Mise à jour des ports ouverts

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10e2455398fc2417df27dde33966ec4bc4b445881d2b6984c98c7725c353cc05
-size 1659
+oid sha256:c1f3e7c3120d3f014c959ac1bb4b5176ee67fb30bbd8f7d8290b6c8b4aa2d605
+size 1645
